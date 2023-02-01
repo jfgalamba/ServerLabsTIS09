c95ac02ae57adfc59c914205bc49b6bc82da62cc
jgalamba <jfgalamba@mail>
2023-02-01 19:55:19 +0000

    More attributes

--- python/gestao_produtos.py
+++ python/gestao_produtos.py
@@ -20,29 +20,61 @@ DEFAULT_INDENTATION = 3
 ##
 ################################################################################
 
+PRODUCT_TYPES = {
+    'AL': 'Alimentação',
+    'DL': 'Detergente p/ Loiça',
+    'FRL': 'Frutas e Legumes'
+}
+
 class Produto:
-    def __init__(self, id: int, nome: str):
+    def __init__(
+            self, 
+            id: int, 
+            nome: str, 
+            tipo: str, 
+            quantidade: int,
+            preco: dec,
+    ):
         if id < 0 or len(str(id)) != 5:
             raise ValueError(f'{id=} inválido (deve ser > 0 e ter 5 dígitos)')
+        if not nome:
+            raise ValueError(f'Nome vazio')
+        # if len(nome) == 0:
+        #     raise ValueError(f'Nome vazio')
+        if quantidade < 0:
+            raise ValueError(f'{quantidade=} inválida (deve ser > 0)')
+        if tipo not in PRODUCT_TYPES:
+            raise ValueError(f'{tipo=} não reconhecido')
+        if preco < 0:
+            raise ValueError(f'{preco=} inválido (deve ser > 0)')
+
         self.id = id
         self.nome = nome
+        self.tipo = tipo
+        self.quantidade = quantidade
+        self.preco = preco
     #:
-    
+
+    @property
+    def desc_tipo(self):
+        return PRODUCT_TYPES[self.tipo]
+    #:
+
     def __str__(self):
         return f"Produto[id: {self.id} nome: {self.nome}]"
     #:
 #:
 
 def main():
-    prod1 = Produto(30987, "pão de milho")
-    prod2 = Produto(30098, "leite mimosa")
+    prod1 = Produto(30987, "pão de milho", 'AL', 2, dec('1'))
+    prod2 = Produto(30098, "leite mimosa", 'AL', 10, dec('2'))
 
     print(prod1)
     print(prod2)
 
     try:
-        Produto(398, "leite mimosa")
-    except ValueError as ex:
+        Produto(30098, "leite mimosoa", 'AL', 6, dec('-10'))
+    except ValueError as ex:    
         print("ATENÇÃO: Produto inválido!")
         print(ex)
 #:
